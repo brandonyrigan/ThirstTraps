f5fd368cd953fdff9280733e55468f18049e58b2
brandonyrigan <brandonayrigan@gmail.com>
2020-03-18 17:04:28 -0700

    Updates

---
+++ precache-manifest.dfcbb39b1e2f845626d9bcd13a04ccfb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6647e33af7a8e28b628a36b1f757382a",
+    "url": "/thirsttraps/index.html"
+  },
+  {
+    "revision": "b8a174f9fea9b4201ca6",
+    "url": "/thirsttraps/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "5b383868ba0b6796c19c",
+    "url": "/thirsttraps/static/css/main.9d9ce323.chunk.css"
+  },
+  {
+    "revision": "b8a174f9fea9b4201ca6",
+    "url": "/thirsttraps/static/js/2.c9cf40d7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/thirsttraps/static/js/2.c9cf40d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b383868ba0b6796c19c",
+    "url": "/thirsttraps/static/js/main.f078da02.chunk.js"
+  },
+  {
+    "revision": "c579e5669faccbee9dd4",
+    "url": "/thirsttraps/static/js/runtime-main.699f68a6.js"
+  },
+  {
+    "revision": "a87a3a38c14bb345780b7fcb51b4a726",
+    "url": "/thirsttraps/static/media/iphone.a87a3a38.png"
+  }
+]);
